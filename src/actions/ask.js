a2fffc5a0e8a28bf9941b748932178a16c26e2bf
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-08-11 00:19:21 +0200

    ask question wip

--- src/actions/ask.js
+++ src/actions/ask.js
@@ -13,17 +13,36 @@ export default (query) => { // will pass classId
 
     api.app.authenticate()
       .then(() => {
-        backend.find(
-          query: { ask: true }
-        ) // will pass classId
+        backend.find() // will pass classId
           .then((result) => {
 
             dispatch({
               type: QUESTION_ASKED,
               payload: result
             })
-            console.log(result)
-            history.push(`/students/${result._id}`)
+
+            const greenStudents = result.data.filter((student) => {return student.currentColor === 'green';});
+            const yellowStudents = result.data.filter((student) => {return student.currentColor === 'yellow';});
+            const redStudents = result.data.filter((student) => {return student.currentColor === 'red';});
+            function pickStudentFromGroup(array) {return array[Math.floor(Math.random() * array.length)];}
+
+            const randomStudent = function () {
+              var number = Math.floor(Math.random() * 100 + 1);
+              switch (true) {
+              case (number < 51) :
+                return pickStudentFromGroup(redStudents);
+              case (number < 84) :
+                return pickStudentFromGroup(yellowStudents);
+              case (number > 83) :
+                return pickStudentFromGroup(greenStudents);
+              default :
+                return 'Oh boy something went wrong';
+              }
+            }
+
+            console.log(randomStudent)
+            debugger;
+            history.push(`/students/${randomStudent._id}`)
           })
           .catch((error) => {
 
